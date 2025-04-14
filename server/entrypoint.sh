a3757d1b2f3e11240c489d172880b573e000ba68
Excel619 <excel8392@gmail.com>
2025-04-14 12:13:46 -0600

    Fixed bug with overlay inflation

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -8,7 +8,7 @@ inflate() {
 
   mkdir -p "$dest"
 
-  for file in "$src"/*; do
+  find "$src" -maxdepth 1 -type f | while read -r file; do
     base=$(basename "$file")
     path=$(echo "$base" | sed 's/__/\//g')
     mkdir -p "$dest/$(dirname "$path")"
@@ -21,7 +21,7 @@ inflate /overlays/base /inflated/base
 inflate /overlays/env /inflated/env
 
 # Inject the configuration files from config volume into paper
-./palimpsest -o /opt/paper -o /overlays/base -o /overlays/env -t /opt/paper
+./palimpsest -o /opt/paper -o /inflated/base -o /inflated/env -t /opt/paper
 
 exec java -Xms2048M -Xmx2048M -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 -XX:SurvivorRatio=32 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper.jar nogui &
 pid=$!
